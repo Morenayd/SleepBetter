57e59ca4262eec798ff90ac071ce102bc443d872
Jesutofunmi <tofdebby@gmail.com>
2024-02-14 07:50:43 +0100

    Initial Commit

---
+++ Sleep Better/Sleep_BetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_BetterApp.swift
+//  Sleep Better
+//
+//  Created by Jesutofunmi Adewole on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_BetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
